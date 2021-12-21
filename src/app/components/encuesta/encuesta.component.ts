ec7be5ef80a6c355fa53a1e8da2c521585bdb686
alvaromoller <alvaromollerlima@gmail.com>
2021-12-20 22:20:42 -0400

    probando filter.pipe en component encuesta

--- src/app/components/encuesta/encuesta.component.ts
+++ src/app/components/encuesta/encuesta.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from "@angular/platform-browser";
+import { ProductosService } from '../../servicios/productos.service';
+
 
 @Component({
   selector: 'app-encuesta',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncuestaComponent implements OnInit {
 
-  constructor() { }
+  //Filter, para las busquedas
+  filterPost = "";
+  
+  constructor(private _productosService:ProductosService,
+              private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    this.getProducts();
   }
+
+  //lista de productos
+  productos:any;
+  getProducts(){
+    this._productosService.getProducts()
+    .subscribe(data => {
+      this.productos = data;
+    });
+  } 
+
+  //metodo para obtener imagen de la base de datos
+  public getImgUrl(url : string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
 
 }
--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -3,25 +3,35 @@ import { Pipe, PipeTransform } from '@angular/core';
 export interface TablaProductos{
   productId: number,
   name: string,
-  description: string
+  description: string,
+  price: number
 }
 @Pipe({
   name: 'filter'
 })
 
 export class FilterPipe implements PipeTransform {
-  // public arrProductos = new Array<TablaProductos>(); 
+
+  //public arrProductos = new Array<TablaProductos>(); 
+  //post.price.indexOf(arg) > -1
+  
   transform(arrProductos: any[], arg: any): any {
-    const resltadoPost = [];  // TODO Sergio Array Vacio
-    if(arrProductos !== undefined){
-      for (const post of arrProductos){
-        if (post.name.toLowerCase().indexOf(arg.toLowerCase()) > -1 ){
+    const resltadoPost = [];  // TODO  Array Vacio
+    if( arg.length < 3) return arrProductos;      //la busqueda debe tener por lo menos 3 letras
+    if(arrProductos !== undefined ){
+      for (const post of arrProductos){ //busqueda post por name, description, price
+        if (post.name.toLowerCase().indexOf(arg.toLowerCase()) > -1  || 
+            post.description.toLowerCase().indexOf(arg.toLowerCase()) > -1 
+           ) {
+
             console.log("SI");
             resltadoPost.push(post); 
         }
       }
     }
-    return resltadoPost; // TODO Sergio Retorna el arrat con las coincidencias
+    return resltadoPost; // TODO  Retorna el array con las coincidencias
   }
+
+  
 }
 
